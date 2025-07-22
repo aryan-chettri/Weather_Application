eb5e9e9f907cc573ff873f6df19d2456f0e1acf5
Aryan Chettri <aryanchettri17@gmail.com>
2025-07-22 18:18:40 +0530

    The basic structure of the application is complete and ready to be deployed

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 const apiKey = "03d72d9c441a4f41eaae1af9f56a55e9";
 
 const apiUrl =
+  "https://api.openweathermap.org/data/2.5/weather?q=";
+const apiUrl1 =
   "https://api.openweathermap.org/data/2.5/weather?q=kolkata&units=metric";
 
 
 async function checkWeather(){
-    const response = await fetch(apiUrl + `&appid=${apiKey}`);
-    const data = await response.json();
 
-    console.log(data); 
+  const inputField = document.getElementById('cityInput');
 
-    document.querySelector('.display').innerHTML = data.main.humidity;
+  console.log(inputField.value);
+
+  console.log(inputField);
+
+  const response = await fetch(apiUrl+`${inputField.value}`+`&appid=${apiKey}`+`&units=metric`);
+  const data = await response.json();
+
+  console.log(data);
+  
+  document.querySelector('#cityName').innerHTML= inputField.value;
+  document.querySelector('#temperature').innerHTML = `${data.main.temp}°C`;
+  document.querySelector('#wind-speed').innerHTML = `${data.wind.speed}km/hrs`;
+
+  // document.querySelector('.display').innerHTML = data.main.humidity;
 }
 
-checkWeather();
+
